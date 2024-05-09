028514a87d6079f93fce9a3970c65badd27dc0ba
leeboa2005 <leeboa2005@naver.com>
2024-05-09 16:27:55 +0900

    튜터님 피드백 적용

--- modules/rolling_slider.js
+++ modules/rolling_slider.js
@@ -1,8 +1,8 @@
-import fetchMoviesData from './apiData.js';
+import fetchMoviesData from "./apiData.js";
 
 async function getData() {
   try {
-    const data = await fetchMoviesData('top_rated');
+    const data = await fetchMoviesData("top_rated");
 
     displayMoviePosters(data.results);
     clone();
@@ -12,14 +12,14 @@ async function getData() {
 }
 getData();
 
-const rollingMovieSlides = document.querySelector('.rolling_movie_slides');
+const rollingMovieSlides = document.querySelector(".rolling_movie_slides");
 
 // 포스터 생성
 function displayMoviePosters(movies) {
   movies.forEach((movie) => {
     const moviePosterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     rollingMovieSlides.insertAdjacentHTML(
-      'beforeend',
+      "beforeend",
       `<li>
   <a href="review.html?id=${movie.id}" target="_blank" aria-label="${movie.title} 리뷰 보기">
     <img src="${moviePosterUrl}" alt="${movie.title} 포스터">
@@ -32,62 +32,58 @@ function displayMoviePosters(movies) {
   });
 }
 
-const rmPlayBtn = document.querySelector('.rm_stop_btn');
-const rmStopBtn = document.querySelector('.rm_play_btn');
+const rmStopBtn = document.querySelector(".rm_stop_btn");
+const rmPlayBtn = document.querySelector(".rm_play_btn");
 const rollingMovieWrap = document.querySelector(
-  '.rolling_movie_slides_wrapper'
+  ".rolling_movie_slides_wrapper"
 );
 
 // rolling_movie_slides를 복사하는 기능
 function clone() {
   const clone = rollingMovieSlides.cloneNode(true);
   rollingMovieWrap.appendChild(clone);
-
-  rollingMovieSlides.offsetWidth + 'px';
-
-  rollingMovieSlides.classList.add('original');
-  clone.classList.add('clone');
+  rollingMovieSlides.classList.add("original");
+  clone.classList.add("clone");
 }
 
 // 애니메이션 재생 기능
 function animationRunning(run) {
-  // 변수 추가해서 넣기
-  const rmOriginal = document.querySelector('.rolling_movie_slides.original');
-  const rmClone = document.querySelector('.rolling_movie_slides.clone');
+  const rmOriginal = document.querySelector(".rolling_movie_slides.original");
+  const rmClone = document.querySelector(".rolling_movie_slides.clone");
   rmOriginal.style.WebkitAnimationPlayState = `${run}`;
   rmClone.style.WebkitAnimationPlayState = `${run}`;
 }
 
 // 애니메이션 정지 기능
 function animationPaused(stp) {
-  const rmOriginal = document.querySelector('.rolling_movie_slides.original');
-  const rmClone = document.querySelector('.rolling_movie_slides.clone');
+  const rmOriginal = document.querySelector(".rolling_movie_slides.original");
+  const rmClone = document.querySelector(".rolling_movie_slides.clone");
   rmOriginal.style.WebkitAnimationPlayState = `${stp}`;
   rmClone.style.WebkitAnimationPlayState = `${stp}`;
 }
 
 //클릭했을때 재생, 정지 이벤트
-rmStopBtn.addEventListener('click', (e) => {
+rmPlayBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  animationRunning('running');
-  rmStopBtn.classList.remove('active');
-  rmPlayBtn.classList.add('active');
+  animationRunning("running");
+  rmPlayBtn.classList.remove("active");
+  rmStopBtn.classList.add("active");
 });
 
-rmPlayBtn.addEventListener('click', (e) => {
+rmStopBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  animationPaused('paused');
-  rmPlayBtn.classList.remove('active');
-  rmStopBtn.classList.add('active');
+  animationPaused("paused");
+  rmStopBtn.classList.remove("active");
+  rmPlayBtn.classList.add("active");
 });
 
 //마우스 오버했을때 재생, 정지 이벤트
-rollingMovieWrap.addEventListener('mouseenter', () => {
-  animationPaused('paused');
+rollingMovieWrap.addEventListener("mouseenter", () => {
+  animationPaused("paused");
 });
 
-rollingMovieWrap.addEventListener('mouseleave', () => {
-  if (rmPlayBtn.classList.contains('active')) {
-    animationRunning('running');
+rollingMovieWrap.addEventListener("mouseleave", () => {
+  if (rmStopBtn.classList.contains("active")) {
+    animationRunning("running");
   }
 });
